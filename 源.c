bad15f26f31330c7fb853958a80249c3d1dea083
BleachG <122334045+BleachG@users.noreply.github.com>
2023-01-11 10:59:50 +0800

    Add files via upload

--- 源.c
+++ 源.c
@@ -1,44 +1,30 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-void seach_s(char* s, char x)
+floate caulate(int x, int y,char a)
 {
-	int i = 0;
-	int m = strlen(s);
-	int flag = 0;
-	for (i = 0; *s != '\0'; i++)
-	{
-		if (*s == x)
-		{
-			flag = 1;
-			break;
-		}
-		else
-		{
-			s++;
-		}
-	}
-	int j = 0;
-	if (flag == 1)
-	{
-		for (j = i; *s != '\0'; j++)
-		{
-			printf("%c", *s);
-			s++;
-		}
-	}
-	else
-	{
-		printf("Not found");
-	}
+    float c = 0;
+    if (a == '+')
+    {
+         c = a + b;
+    }
+    if (a == '-')
+    {
+         c = a -b;
+    }if (a == '*')
+    {
+        int c = a * b;
+    }if (a == '/')
+    {
+        int c = a +/b;
+    }
+    return c;
 }
 int main()
 {
-	char a[80];
-	gets(a);
-	char c;
-	scanf("%c", &c);
-	seach_s(a, c);
-	return 0;
+    float a, b, c;
+    char d, e;
+    scanf("%f%f%f%c%c", &a, &b, &c, &d, &e);
+    caulate(a, b,d);
+    float z = caulate(caulate(a, b, d), c, e);
+    printf("%d\n", z);
 }
